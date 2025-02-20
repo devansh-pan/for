9824f2b148cfde18740eeee35cd6ab73717d3b3f
Krishna <143727212+devansh-pan@users.noreply.github.com>
2025-02-20 09:43:05 +0000

    hell

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,9 +1,13 @@
 
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from "$env/static/public"
 
 const supabaseUrl = PUBLIC_SUPABASE_URL;
 const supabaseKey = PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+        global: {
+          fetch,
+        },
+      });
         
